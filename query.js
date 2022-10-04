c75a38a91fae02befc248715cc0a63e809c1eb37
12AnubiS12 <kirillov974@gmail.com>
2022-10-04 18:21:10 +0300

    fix2

--- query.js
+++ query.js
@@ -2,6 +2,13 @@ $(document).ready(function(){
     var i = 0;
     $('input[type="text"]').keypress(function(){
         $("span").text(i += 1);
-        $("p").show().fadeOut();
+        $(".h1").show().fadeOut();
+    });
+    $("button").click(function(){
+        $(".box").animate({
+            width: 'toggle'
+        });
     });
 });
+
+
